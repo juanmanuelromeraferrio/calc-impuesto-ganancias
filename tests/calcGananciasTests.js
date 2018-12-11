dd597dd79f0fc9b4e2419d6cc50816eeb4ab4c09
Juan M. Romera <juanmanuel.romeraferrio@gmail.com>
2018-12-11 22:05:48 +0100

    clean code

--- tests/calcGananciasTests.js
+++ tests/calcGananciasTests.js
@@ -1,73 +1,69 @@
-'use strict';
+"use strict";
 
-var expect = require('chai').expect;
-var CalcGanancias = require('../index');
+var expect = require("chai").expect;
+var CalcGanancias = require("../index");
 
-describe('#CalcGanancias.calculate', function () {
-    it('should return no taxes if salary is 0', function () {
+describe("#CalcGanancias.calculate", function() {
+  it("should return no taxes if salary is 0", function() {
+    var options = {
+      sueldoBruto: 0,
+      alquiler: 0,
+      creditoHipotecario: 0,
+      cantHijos: 0,
+      isConyuje: false,
+      isJubilado: false,
+      isPatagonico: false
+    };
 
-        var options = {
-            sueldoBruto: 0,
-            alquiler: 0,
-            creditoHipotecario: 0,
-            cantHijos: 0,
-            isConyuje: false,
-            isJubilado: false,
-            isPatagonico: false
-        }
+    var calcGanancias = new CalcGanancias();
+    var result = calcGanancias.calculate(options);
 
-        var calcGanancias = new CalcGanancias();
-        var result = calcGanancias.calculate(options);
+    expect(result.anualTax).to.equal("0.00");
+    expect(result.monthlyTax).to.equal("0.00");
+    expect(result.taxRate).to.equal("0.00%");
+    expect(result.marginalTaxRate).to.equal("0.00%");
+    expect(result.netBaseSalary).to.equal("0.00");
+  });
 
-        expect(result.anualTax).to.equal("0.00");
-        expect(result.monthlyTax).to.equal("0.00");
-        expect(result.taxRate).to.equal("0.00%");
-        expect(result.marginalTaxRate).to.equal("0%");
-        expect(result.netBaseSalary).to.equal(0);
-    });
+  it("should return no taxes if salary is 10000 and net salary has to be 8300", function() {
+    var options = {
+      sueldoBruto: 10000,
+      alquiler: 0,
+      creditoHipotecario: 0,
+      cantHijos: 0,
+      isConyuje: false,
+      isJubilado: false,
+      isPatagonico: false
+    };
 
-    it('should return no taxes if salary is 10000 and net salary has to be 8300', function () {
+    var calcGanancias = new CalcGanancias();
+    var result = calcGanancias.calculate(options);
 
-        var options = {
-            sueldoBruto: 10000,
-            alquiler: 0,
-            creditoHipotecario: 0,
-            cantHijos: 0,
-            isConyuje: false,
-            isJubilado: false,
-            isPatagonico: false
-        }
+    expect(result.anualTax).to.equal("0.00");
+    expect(result.monthlyTax).to.equal("0.00");
+    expect(result.taxRate).to.equal("0.00%");
+    expect(result.marginalTaxRate).to.equal("0.00%");
+    expect(result.netBaseSalary).to.equal("8300.00");
+  });
 
-        var calcGanancias = new CalcGanancias();
-        var result = calcGanancias.calculate(options);
+  it("should return no taxes if salary is 10000 and net salary has to be 9400 if is jubilado", function() {
+    var options = {
+      sueldoBruto: 10000,
+      alquiler: 0,
+      creditoHipotecario: 0,
+      cantHijos: 0,
+      isConyuje: false,
+      isJubilado: true,
+      isPatagonico: false
+    };
 
-        expect(result.anualTax).to.equal("0.00");
-        expect(result.monthlyTax).to.equal("0.00");
-        expect(result.taxRate).to.equal("0.00%");
-        expect(result.marginalTaxRate).to.equal("0%");
-        expect(result.netBaseSalary).to.equal(8300);
-    });
+    var calcGanancias = new CalcGanancias();
+    var result = calcGanancias.calculate(options);
 
-    it('should return no taxes if salary is 10000 and net salary has to be 9400 if is jubilado', function () {
-
-        var options = {
-            sueldoBruto: 10000,
-            alquiler: 0,
-            creditoHipotecario: 0,
-            cantHijos: 0,
-            isConyuje: false,
-            isJubilado: true,
-            isPatagonico: false
-        }
-
-        var calcGanancias = new CalcGanancias();
-        var result = calcGanancias.calculate(options);
-
-        expect(result.anualTax).to.equal("0.00");
-        expect(result.monthlyTax).to.equal("0.00");
-        expect(result.taxRate).to.equal("0.00%");
-        expect(result.marginalTaxRate).to.equal("0%");
-        expect(result.netBaseSalary).to.equal(9400);
-    });
-
+    expect(result.anualTax).to.equal("0.00");
+    expect(result.monthlyTax).to.equal("0.00");
+    expect(result.taxRate).to.equal("0.00%");
+    expect(result.marginalTaxRate).to.equal("0.00%");
+    expect(result.netBaseSalary).to.equal("9400.00");
+  });
 });
